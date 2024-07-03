9bbf3484de0427c339b3f0035be3915921f220a0
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2024-07-03 09:47:59 +0200

    Update ComprarEntradasView.php

--- concert-app/view/entradas/ComprarEntradasView.php
+++ concert-app/view/entradas/ComprarEntradasView.php
@@ -10,7 +10,7 @@
             <!-- CREACIÓN DE FORMULARIO -->
             <form action="index.php?controlador=entradas&action=add" method=post>
                 <fieldset>
-                    <legend><h2>COMPRA DE ENTRADAS 2.0.0!!</h2></legend>
+                    <legend><h2>COMPRA DE ENTRADAS TEST!!</h2></legend>
                     <label>Nombre:</label><br/>
                     <!-- En el value indico con php que me inserte el valor en el input en caso de que hubiese -->
                     <input type="text" name="nombre" value="<?php echo $dF['valores']['nombre'];?>" /><br />
